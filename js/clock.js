fb755c97a68e6ac8c8f3af9b014222d159c13af1
Mikkel Jørgensen <95000922+ExManubis@users.noreply.github.com>
2025-01-03 19:58:25 +0100

    feat(easter-egg): add easter-egg
    Added fun retro 3.0 wallpaper easter-egg.

--- js/clock.js
+++ js/clock.js
@@ -1,8 +1,14 @@
 // CONSTANTS
 const time = document.querySelector(".time");
+const bodyQS = document.querySelector("body");
 
 // SCRIPT
 setInterval(() => {
   let now = new Date();
   time.textContent = now.toLocaleTimeString("da-DK");
 }, 1000);
+
+// EASTER EGG
+time.addEventListener("click", () => {
+  body.classList.toggle("retro");
+});
